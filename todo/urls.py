80d1e43f6ec27cc2a0e411dd4200686fe1fe208c
sua <dssua0714@naver.com>
2025-03-21 16:34:27 +0900

    완료 및 완료 목록

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_todo, complete_todo, todo_list
+from .views import add_todo, complete_todo, completed_todos, todo_list
 
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", todo_list, name="todo_list"),  # dev_2
     # 추가 기능을 위한 URL
     path('add/', add_todo, name='add_todo'), # dev_3
-    # 완료 기능을 위한 URL
+    # 완료 기능을 위한 URL (/todo/complete/1/)
     path('complete/<int:todo_id>/', complete_todo, name='complete_todo'),  # dev_4
+    path('completed/', completed_todos, name='completed_todos'),    # dev_4
 ]
--- todo/views.py
+++ todo/views.py
@@ -6,7 +6,10 @@ from todo.models import Todo
 
 # dev_2
 def todo_list(request) : 
-    todos = Todo.objects.all()   # 모든 todo 객체를 가져옴
+    # dev_2
+    # todos = Todo.objects.all()   # 모든 todo 객체를 가져옴
+    # dev_4
+    todos = Todo.objects.filter(is_completed = False)   # 미완료 todo 객체만 필터링
     return render(request, "todo/todo_list.html", {"todos" : todos})    # todo_list.html 파일을 렌더링하면서 할 일 목록을 전달
 
 # dev_3 
@@ -14,7 +17,8 @@ def add_todo(request) :
     # 사용자가 POST 요청을 보내면, 입력한 제목을 받아와서 새로운 할 일을 데이터베이스에 저장
     if request.method == "POST" : 
         title = request.POST.get("title")   # 폼에서 입력된 title 가져오기
-        
+        if request.POST.is_valid() : 
+            pass
         if title : 
             Todo.objects.create(title=title)    # title이 비어있지 않으면 새 할 일 저장
             print("할 일이 정상적으로 추가됨!")  # 터미널에서 확인
@@ -31,8 +35,13 @@ def complete_todo(request, todo_id) :
     # 지정한 객체를 가져오되, 없으면 404 에러
     # http://127.0.0.1:8000/todo/complete/999/ => id=999인 todo가 없으면 에러
     # get_object_or_404를 통해 자동으로 404 페이지로 이동
-    
+
     todo = get_object_or_404(Todo, id=todo_id)
     todo.is_completed = True
     todo.save()
     return redirect("todo_list")
+
+def completed_todos(request):
+    todos = Todo.objects.filter(is_completed=True)  # 완료순
+    # todos = Todo.objects.filter(is_completed=True).order_by('-created_at')    # 생성순
+    return render(request, "todo/completed_list.html", {"todos": todos})
