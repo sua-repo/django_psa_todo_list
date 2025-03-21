593ec9845235b19059486a906e90f209e90bc121
sua <dssua0714@naver.com>
2025-03-21 14:47:48 +0900

    할일추가

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import todo_list
+from .views import add_todo, todo_list
 
 
 urlpatterns = [
     # 127.0.0.1:8000/todo/에서 할 일 목록을 볼 수 있도록 설정
     path("", todo_list, name="todo_list"),  # dev_2
+    # 추가 기능을 위한 URL
+    path('add/', add_todo, name='add_todo'), # dev_3
     
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from todo.models import Todo
 
@@ -9,3 +9,18 @@ def todo_list(request) :
     todos = Todo.objects.all()   # 모든 todo 객체를 가져옴
     return render(request, "todo/todo_list.html", {"todos" : todos})    # todo_list.html 파일을 렌더링하면서 할 일 목록을 전달
 
+# dev_3 
+def add_todo(request) : 
+    # 사용자가 POST 요청을 보내면, 입력한 제목을 받아와서 새로운 할 일을 데이터베이스에 저장
+    if request.method == "POST" : 
+        title = request.POST.get("title")   # 폼에서 입력된 title 가져오기
+        
+        if title : 
+            Todo.objects.create(title=title)    # title이 비어있지 않으면 새 할 일 저장
+            print("할 일이 정상적으로 추가됨!")  # 터미널에서 확인
+        else:
+            print("제목이 비어 있어서 추가되지 않음!")  # 터미널에서 확인
+        
+        return redirect("todo_list")
+    
+    return render(request, "todo/add_todo.html")  # GET 요청 시 폼을 보여줌
