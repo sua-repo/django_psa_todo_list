7ee70fc67260fcef032c58bb145fefd3b5c1101a
sua <dssua0714@naver.com>
2025-07-22 18:54:06 +0900

    Fix: Update root URL to redirect to /todo/

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
+
+def root_redirect(request):
+    return redirect('/todo/')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', root_redirect),
     # 'todo/' 경로로 들어오면 todo.urls에서 처리하도록 연결
     path("todo/", include("todo.urls")),    # dev_2
     
